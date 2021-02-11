651a9f2a39b9ea6a50d4937289fb20f317cb1012
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-02-11 21:16:33 +0000

    Update

--- test/unit-test/FreeRTOS_IP_utest.c
+++ test/unit-test/FreeRTOS_IP_utest.c
@@ -16,9 +16,20 @@
 
 #include "FreeRTOS_IP.h"
 #include "FreeRTOS_IP_Private.h"
+#include "FreeRTOS_ARP.h"
+#include "FreeRTOS_DNS.h"
 
 /* Include the mocked header files. */
+#include "mock_FreeRTOS_Sockets.h"
+#include "mock_FreeRTOS_ARP.h"
+#include "mock_FreeRTOS_UDP_IP.h"
+#include "mock_FreeRTOS_DHCP.h"
+#include "mock_NetworkInterface.h"
+#include "mock_FreeRTOS_DNS.h"
+#include "mock_queue_annexed.h"
 #include "mock_NetworkBufferManagement.h"
+
+#include "/home/ubuntu/Desktop/CMockunittest/test/unit-test/build/FreeRTOS_TCP_Annexed/FreeRTOS_TCP_annex.h"
 
 void test_pxGetNetworkBufferWithDescriptor( void )
 {
@@ -49,7 +60,29 @@ void test_pxGetNetworkBufferWithDescriptor( void )
     TEST_ASSERT_EQUAL( pvResult, NULL );
 }
 
+#define DEF_ADDR_VALS 0x1234
+
 void test_FreeRTOS_GetAddressConfiguration( void )
 {
+    uint32_t ulIPAddress = DEF_ADDR_VALS;
+    uint32_t ulNetMask = DEF_ADDR_VALS;
+    uint32_t ulGatewayAddress = DEF_ADDR_VALS;
+    uint32_t ulDNSServerAddress = DEF_ADDR_VALS;
 
+    FreeRTOS_GetAddressConfiguration( NULL, NULL, NULL, NULL );
+    TEST_ASSERT_EQUAL( ulIPAddress, DEF_ADDR_VALS );
+    TEST_ASSERT_EQUAL( ulNetMask, DEF_ADDR_VALS );
+    TEST_ASSERT_EQUAL( ulGatewayAddress, DEF_ADDR_VALS );
+    TEST_ASSERT_EQUAL( ulDNSServerAddress, DEF_ADDR_VALS );
+
+    FreeRTOS_GetAddressConfiguration( &ulIPAddress, &ulNetMask, &ulGatewayAddress, &ulDNSServerAddress );
+    TEST_ASSERT_EQUAL( ulIPAddress, 0 );
+    TEST_ASSERT_EQUAL( ulNetMask, 0 );
+    TEST_ASSERT_EQUAL( ulGatewayAddress, 0 );
+    TEST_ASSERT_EQUAL( ulDNSServerAddress, 0 );
+}
+
+void test_prvProcessNetworkDownEvent( void )
+{
+    prvProcessNetworkDownEvent();
 }
--- test/unit-test/ModuleCmake/IP_c_buld.cmake
+++ test/unit-test/ModuleCmake/IP_c_buld.cmake
@@ -5,8 +5,15 @@ function(create_ip_c_utest target)
     # List the files required for the build
     list( APPEND real_source_dirs
              ${TCP_ANNEXED_SOURCES}
+             ${MODULE_ROOT_DIR}/FreeRTOS_IP.c
              ${KERNEL_SOURCES}
              ${MODULE_ROOT_DIR}/test/unit-test/stubs/FreeRTOS_ARP_stubs.c )
+
+    list( REMOVE_ITEM real_source_dirs
+             "${MODULE_ROOT_DIR}/test/FreeRTOS-Kernel/tasks.c"
+             "${MODULE_ROOT_DIR}/test/FreeRTOS-Kernel/queue.c")
+
+    message( STATUS "Real source directories: ${real_source_dirs}" )
 
     # list the files to mock here
     list(APPEND ${target}_mock_list
@@ -17,12 +24,16 @@ function(create_ip_c_utest target)
                 "${MODULE_ROOT_DIR}/include/NetworkInterface.h"
                 "${MODULE_ROOT_DIR}/include/NetworkBufferManagement.h"
                 "${MODULE_ROOT_DIR}/include/FreeRTOS_DNS.h"
+#               Kernel mocks
+                "${ANNEXED_DIR}/queue_annexed.h"
+                "${ANNEXED_DIR}/task_annexed.h"
             )
 
     # list the directories your mocks need
     list(APPEND ${target}_mock_include_list
                 .
                 ${MODULE_ROOT_DIR}/test/unit-test/ConfigFiles
+                ${MODULE_ROOT_DIR}/test/FreeRTOS-Kernel/include
                 ${KERNEL_INCLUDE_DIRS}
                 ${MODULE_ROOT_DIR}/include
                 ${MODULE_ROOT_DIR}/tools/CMock/src
@@ -53,13 +64,11 @@ function(create_ip_c_utest target)
 
     list(APPEND utest_link_list
                 lib${${target}_real_name}.a
-                -lcmock
-                -lunity
+                -l${mock_name}
             )
 
     list(APPEND utest_dep_list
                 ${${target}_real_name}
-                -l${mock_name}
             )
 
     set(utest_name "${utest_module_name}_utest")
@@ -68,6 +77,7 @@ function(create_ip_c_utest target)
     list(APPEND test_include_directories
                 ${MODULE_ROOT_DIR}/test/unit-test/ConfigFiles
                 ${KERNEL_INCLUDE_DIRS}
+                ${MODULE_ROOT_DIR}/test/FreeRTOS-Kernel/include
                 ${MODULE_ROOT_DIR}/include)
 
     create_test(${utest_name}
